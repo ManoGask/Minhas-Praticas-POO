c2f3b5fef112db952e3dd824e0f799f277fae209
ManoGask <143042231+ManoGask@users.noreply.github.com>
2023-09-28 19:55:11 -0300

    Add files via upload

---
+++ Pratica4/src/questao2/GeraExtrato.java
@@ -0,0 +1,7 @@
+package questao2;
+
+public class GeraExtrato {
+    public void geradorConta(Conta conta){
+        System.out.println("Saldo Atual: "+conta.getSaldo());
+    }
+}
---
+++ Pratica4/src/questao2/Main.java
@@ -0,0 +1,18 @@
+package questao2;
+
+public class Main {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente();
+        cc.depositar(1200.20);
+        cc.sacar(300);
+
+        ContaPoupanca cp = new ContaPoupanca();
+        cp.depositar(500.50);
+        cp.sacar(25);
+
+        GeraExtrato gerador = new GeraExtrato();
+        gerador.geradorConta(cc);
+        gerador.geradorConta(cp);
+    }
+
+}
