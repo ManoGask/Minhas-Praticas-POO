5f993bf8647b0cf66fbce1dd31f3c78309baf2b5
ManoGask <143042231+ManoGask@users.noreply.github.com>
2023-09-28 20:22:37 -0300

    Delete Pratica4 directory

--- Pratica4/src/questao2/Conta.java
+++
@@ -1,7 +0,0 @@
-package questao2;
-
-public interface Conta {
-    void depositar (double valor);
-    void sacar(double valor);
-    double getSaldo();
-}
--- Pratica4/src/questao2/GeraExtrato.java
+++
@@ -1,7 +0,0 @@
-package questao2;
-
-public class GeraExtrato {
-    public void geradorConta(Conta conta){
-        System.out.println("Saldo Atual: "+conta.getSaldo());
-    }
-}
